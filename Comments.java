6494f677cbc650d171f01d75d65757ae25d747b3
JamesCordova <jcordovas@unsa.edu.pe>
2023-05-07 10:08:12 -0500

    Java Comments usando dos formas de hacer comentarios

---
+++ Comments.java
@@ -0,0 +1,6 @@
+public class Comments{
+	public static void main(String[] args){
+		System.out.println("Para comentar podemos utilizar // o /* */");
+		// Este comentario es de solo una línea, todo lo escrito en esta línea será un comentario
+		/* En esta forma todo lo que este en el interior de los símbolos serán comentario, pudiendo
+		 * ser de varias líneas pero siempre y cuando se cierre */
