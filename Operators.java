7244863c696bd87024c0588c2dca62dff24f4cb9
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-07 01:21:58 -0500

    'Operators.java' primera parte 'operador +'

--- Operators.java
+++ Operators.java
@@ -1,5 +1,18 @@
 public class Operators {
     public static void main(String[] args){
-        
+        System.out.println("Los operadores se utilizan para realizar operacion en variables y valores");
+        //Ejemplo utilizando el operador '+'
+        int x = 100 + 50;
+        System.out.println("Aunque el operador '+' se usa a menudo para sumar dos valores, como en el ejemplo anterior, también se puede usar para sumar una variable y un valor, o una variable y otra variable:");
+        //Ejemplo
+        int suma1 = 100 + 50;
+        int suma2 = suma1 + 250;
+        int suma3  = suma2 + suma2;
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }    
 }
