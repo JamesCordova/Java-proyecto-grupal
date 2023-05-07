d25df00a4235b616ba17348b84973b63fb612e89
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-07 00:24:44 -0500

    'Variables.java' -> Agregando la primera parte de la informacion

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables{
+    public static void main(String[] args){
+        System.out.println("1. Variables Java");
+        System.out.println("Las Variables son contenedores para almacenar valores de datos.");
+        System.out.println("En Java, existen diferentes tipos de variables, por ejemplo");
+        System.out.println("String - Almacena texto, como 'Hola'. Los valores de cadena estan etre comillas dobles");
+        String str = "Hola";
+        System.out.println("int - almacena enteros (numeros enteros), sin decimales, como 123 o -123");
+        int x = 123;
+        System.out.println("double - almacena numeros con decimales, como 19.99 o -19.99");
+        double xdbl = 19.99;
+        System.out.println("char - almacena caracteres individuales, como 'a' o 'B'. Los valores de char estan entre comillas simples");
+        char chr = 'a';
+        System.out.println("boolean - almacena valores con 2 estados: verdadero o falso");
+        boolean flag = true;
+    }
+}
