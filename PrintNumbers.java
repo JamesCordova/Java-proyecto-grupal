0c3065892b0f6f6d09ba7ee97736e3a166a369bf
JamesCordova <jcordovas@unsa.edu.pe>
2023-05-07 09:55:39 -0500

    Java Output - Print Numbers mostrar números y realizar operaciones

---
+++ PrintNumbers.java
@@ -0,0 +1,10 @@
+public class PrintNumber{
+	public static void main(String[] args){
+		System.out.println("Para mostrar números no necesitamos ponerlos dentro de comillas");
+		System.out.println(5);
+		System.out.println(56000);
+		System.out.println("Podemos realizar operaciones matemáticas");
+		System.out.println(1111 + 1234);
+		System.out.println(1111 * 1111);
+	}
+}
