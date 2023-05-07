b05bba9fc3fcf961815b54ef323186ff9d31dd6a
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-07 02:28:48 -0500

    'Math.java' adicion del contenido para el capitulo

--- Math.java
+++ Math.java
@@ -1,6 +1,11 @@
 
 public class Math {
     public static void main(String[] args){
-        
+        System.out.println("La clase Math tiene muchos metodos que permite realizar tareas matematicas con numeros."); 
+        System.out.println("1. Math.max(x,y): encuentra el valor maximo entre x e y"); 
+        System.out.println("2. Math.min(x,y): encuentra el valor minimo entre x e y"); 
+        System.out.println("3. Math.sqrt(x): retorna la raiz cuadrada de x"); 
+        System.out.println("4. Math.abs(x): retorna el valor absoluto de x"); 
+        System.out.println("5. Math.random(): retorna un numero aleatorio entre 0,0 y 1,0"); 
     }
 }
