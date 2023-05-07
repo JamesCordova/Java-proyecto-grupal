8b9c55c9cd8633cb7ca6302edaa7ae584cbd9108
JamesCordova <jcordovas@unsa.edu.pe>
2023-05-07 09:28:27 -0500

    Java Get Started puntos principales

---
+++ GetStarted.java
@@ -0,0 +1,9 @@
+public class GetStarted{
+	public static void main(String[] args){
+		System.out.println("Instalación de Java");
+		System.out.println("Para saber la versión de Java podemos utilizar: 'java -version'");
+		System.out.println("La instalación en Windows requiere establecer la variable de entorno");
+		System.out.println("Para ejecutar el código primero utilizamos: 'javac Main.java' para compilarlo");
+		System.out.println("Luego utilizando: 'java Main' para ejecutarlo");
+	}
+}
