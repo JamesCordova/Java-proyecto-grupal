6e1297701dd3ee3756ad5846e5d1c9b20674a2da
JamesCordova <jcordovas@unsa.edu.pe>
2023-05-07 11:50:16 -0500

    Java For Loop ejemplo de bucle For

---
+++ ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop{
+	public static void main(String[] args){
+		System.out.println("El bucle For tiene 3 sentencias al llamar al método");
+		// Ejemplo
+		for(int i = 0; i <= 10; i += 2){
+			System.out.println(i);
+		}
+
+	}
+}
