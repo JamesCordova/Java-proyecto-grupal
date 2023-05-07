11eb37207d76fcf7225e56ac86ae4dc27b5c2f7c
JamesCordova <jcordovas@unsa.edu.pe>
2023-05-07 09:38:03 -0500

    Java Syntax resumen de lo principal

---
+++ Syntax.java
@@ -0,0 +1,11 @@
+public class Syntax{
+	public static void main(String[] args){
+		System.out.println("Hola mundo");		
+		System.out.println("Puntos a tratar, toda línea de código debe ir dentro de la clase");		
+		System.out.println("La clase debe tener en mayúsculas la primera letra");		
+		System.out.println("Java detecta entre mayúsculas y minúsculas");		
+		System.out.println("El método main es requerido siempre");		
+		System.out.println("La llaves '{}' marcan el inicio y el final de un bloque de código");
+		System.out.println("Debemos terminar las sentencias con ';'");
+	}
+}
