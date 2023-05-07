945c27ab4bb32e2b67d6de1b90eef4744122932a
JamesCordova <jcordovas@unsa.edu.pe>
2023-05-07 11:56:34 -0500

    Java For-Each Loop ejemplo agregado

--- ForLoop.java
+++ ForLoop.java
@@ -4,7 +4,12 @@ public class ForLoop{
 		// Ejemplo
 		for(int i = 0; i <= 10; i += 2){
 			System.out.println(i);
+		}	
+		System.out.println("El bucle For each es usado generalmente con elementos de un array");
+		// Ejemplo
+		String[] autos = {"Volvo", "BMW", "Ford", "Mazda"};
+		for(String i : autos){
+			System.out.println(i);
 		}
-
 	}
 }
